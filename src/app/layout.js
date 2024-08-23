e66c9d69394817afe57c35d790c52e703dbd2827
Yash Gupta <gupta25yash@gmail.com>
2024-08-23 23:22:51 +0530

    s

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,9 +25,16 @@ export default function RootLayout({ children }) {
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:image" content={metadata.image} />
+        <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" />
         <meta property="og:url" content={typeof window !== 'undefined' ? window.location.href : ''} />
         <meta property="og:type" content="website" />
         <link rel="icon" href="/logo.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content={metadata.title} />
+  <meta name="twitter:description" content={metadata.description} />
+  <meta name="twitter:image" content={metadata.image} />
         
       </head>       
       <body className={inter.className}>
